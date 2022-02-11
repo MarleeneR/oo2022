0b40c3fdd24068bba266ebcadb52ba0a74ad1786
MarleeneR <leene02@tlu.ee>
2022-02-11 15:37:26 +0200

    funktsiooni näited

---
+++ 01_funktsioon/Funktsioonid.java
@@ -0,0 +1,31 @@
+public class Funktsioonid {
+    public static void main(String[] args) {
+        System.out.println(taksosõit(3.5));
+
+        double sõiduMaksumus = taksosõit(0.5);
+        System.out.println(sõiduMaksumus);
+
+        prindiForTsükkel(5);
+
+        System.out.println(temperatuuriHinnang(-2));
+        String hinnang = temperatuuriHinnang(-2);
+        System.out.println(hinnang);
+    }
+
+    public static double taksosõit(double h){
+        return 3+0.8*h;  //funktsioon selleks, et midagi saaks
+    }
+    public static void prindiForTsükkel(int kordadeArv){
+        //fori --> genereerin
+        for (int i = 0; i < kordadeArv; i++){
+            System.out.println(i*10);
+        }
+    }
+    public static String temperatuuriHinnang(float temperatuur){
+        if (temperatuur < 0){
+            return "On jääs";
+        }else{
+            return "Ei ole jääs";
+        }
+    }
+}
